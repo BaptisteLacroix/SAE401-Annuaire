f82f375aca4830ceaf4df10811bfcb62376cacec
Baptiste LACROIX <baptiste.lacroix03@gmail.com>
2023-03-04 18:08:25 +0100

    #9 ajout de la traduction

---
+++ static/js/language.js
@@ -0,0 +1,47 @@
+// On window load add eventlistener to the flags
+window.addEventListener("load", function () {
+    // Get the stored language or default to 'en'
+    const storedLanguage = localStorage.getItem("language") || "en";
+    setLanguage(storedLanguage);
+
+    // Add eventlistener to the flags
+    document.addEventListener("click", function (event) {
+        if (event.target.id === "french") {
+            setLanguage("fr");
+        } else if (event.target.id === "english") {
+            setLanguage("en");
+        } else if (event.target.id === "spanish") {
+            setLanguage("sp");
+        }
+    });
+});
+
+function setLanguage(language) {
+    fetch(`../static/translations/${language}.json`)
+        .then(response => response.json())
+        .then(data => {
+            // Store the selected language in local storage
+            localStorage.setItem("language", language);
+
+            // get the active page
+            let page = document.querySelectorAll('[id^="page-"]')[0].id.split("-")[1];
+            for (const [key, value] of Object.entries(data)) {
+                const element = document.getElementById(key);
+                if (element) {
+                    element.innerHTML = value;
+                }
+                if (key === page) {
+                    value.forEach((item) => {
+                        for (const [key2, value2] of Object.entries(item)) {
+                            const element = document.getElementById(key2);
+                            if (element) {
+                                element.innerHTML = value2;
+                            }
+                        }
+                    });
+                }
+            }
+
+        })
+        .catch(error => console.error(error));
+}
