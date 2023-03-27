eb7b35915e68b836a84d467878bbf011d365df77
Mateus Lopes <mateus.lopes@etu.unice.fr>
2023-03-27 14:37:09 +0200

    changement affichage des filtres

--- static/js/language.js
+++ static/js/language.js
@@ -3,7 +3,7 @@ window.addEventListener("load", function () {
     // Get the stored language or default to 'en'
     const storedLanguage = localStorage.getItem("language") || "en";
     setLanguage(storedLanguage);
-
+    
     // Add eventlistener to the flags
     document.addEventListener("click", function (event) {
         if (event.target.id === "french") {
@@ -14,6 +14,7 @@ window.addEventListener("load", function () {
             setLanguage("sp");
         }
     });
+    setEvents();
 });
 
 function setLanguage(language) {
@@ -44,4 +45,49 @@ function setLanguage(language) {
 
         })
         .catch(error => console.error(error));
+}
+
+function setEvents() {
+    let filters = document.getElementsByClassName("filter");
+    for (let i = 0; i < filters.length; i++) {
+        filters[i].addEventListener("click", function () {
+            addOnSearchbar(filters[i].textContent);
+        });
+    }
+}
+
+function addOnSearchbar(element) {
+    let span = document.createElement("span");
+    span.className = "active-filter";
+    span.innerText = element;
+    let searchBar = document.getElementById("filters-used");
+    if (validate(element, searchBar)) {
+        let croix = document.createElement("span");
+        croix.className = "delete-element";
+        croix.innerText = "❌";
+        croix.addEventListener("click", function() {
+            deleteElement(span);
+        })
+        span.appendChild(croix);
+        searchBar.appendChild(span);
+    }
+}
+
+function validate(element, container) {
+    let activeFilters = container.getElementsByClassName("active-filter");
+    if (activeFilters.length == 3) {
+        return false;
+    }
+    for (let i = 0; i < activeFilters.length; i++) {
+        console.log(activeFilters[i].textContent);
+        if (activeFilters[i].textContent == element+"❌") {
+            return false;
+        }
+    }
+    return true;
+}
+
+function deleteElement(element) {
+    let parent = element.parentElement;
+    parent.removeChild(element);
 }
